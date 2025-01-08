c9db02d983a15c6e12bd93402105f9c659a07fa1
Muhammad-Ameen-205 <ameenyh205@gmail.com>
2025-01-08 17:24:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const count=document.querySelector(".count")
+const minusbtn=document.querySelector(".minusbtn")
+const plusbtn=document.querySelector(".plusbtn")
+const changeby=document.querySelector(".changeby")
+const reset=document.querySelector(".reset")
+
+
+minusbtn.addEventListener("click", ()=> {
+    const countvalue=parseInt(count.innerText)
+    const changebyvalue=parseInt(changeby.value)
+    count.innerText=countvalue - changebyvalue
+    
+})
+
+plusbtn.addEventListener("click", ()=> {
+    const countvalue=parseInt(count.innerText)
+    const changebyvalue=parseInt(changeby.value)
+    count.innerText=countvalue + changebyvalue
+    
+})
+
+reset.addEventListener("click",()=>{
+   count.innerText=0
+})
