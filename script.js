7e79e6391b1553bba4b458b8e7b82f7e097720c4
Muhammad-Ameen-205 <ameenyh205@gmail.com>
2025-01-08 17:26:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const count=document.querySelector(".count")
-const minusbtn=document.querySelector(".minusbtn")
-const plusbtn=document.querySelector(".plusbtn")
-const changeby=document.querySelector(".changeby")
-const reset=document.querySelector(".reset")
+const count=document.querySelector(".count");
+const minusbtn=document.querySelector(".minusbtn");
+const plusbtn=document.querySelector(".plusbtn");
+const changeby=document.querySelector(".changeby");
+const reset=document.querySelector(".reset");
 
 
 minusbtn.addEventListener("click", ()=> {
@@ -10,15 +10,15 @@ minusbtn.addEventListener("click", ()=> {
     const changebyvalue=parseInt(changeby.value)
     count.innerText=countvalue - changebyvalue
     
-})
+});
 
 plusbtn.addEventListener("click", ()=> {
     const countvalue=parseInt(count.innerText)
     const changebyvalue=parseInt(changeby.value)
     count.innerText=countvalue + changebyvalue
     
-})
+});
 
 reset.addEventListener("click",()=>{
    count.innerText=0
-})
+});
